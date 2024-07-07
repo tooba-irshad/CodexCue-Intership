f11c8b673719c7905113e50770f56cc8b42f4b4e
tooba-irshad <toobairshad30@gmail.com>
2024-07-07 15:44:41 +0500

    Initial commit

---
+++ wp-content/themes/neve/vendor/composer/installed.php
@@ -0,0 +1,43 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeinwp/neve',
+        'pretty_version' => 'v3.8.7',
+        'version' => '3.8.7.0',
+        'reference' => 'c7863930e1f73751144afb74ff839cf83f815f7b',
+        'type' => 'wordpress-theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/neve' => array(
+            'pretty_version' => 'v3.8.7',
+            'version' => '3.8.7.0',
+            'reference' => 'c7863930e1f73751144afb74ff839cf83f815f7b',
+            'type' => 'wordpress-theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => '3.3.18',
+            'version' => '3.3.18.0',
+            'reference' => '5463d7170ed7b9735223d6715442b8670a477688',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wptt/webfont-loader' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '68c93e1dc8f6e438f93e81ebdf632e6243e43e0f',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../wptt/webfont-loader',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
